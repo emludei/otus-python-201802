4cedf4d1c193d50d4e551c839f801f2502fcd2c9
Alexander Tankeev <incontrol64@yandex.ru>
2018-03-10 23:26:14 +0300

    refactoring after code review

--- hw1/log_analizer/log_analyzer.py
+++ hw1/log_analizer/log_analyzer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # log_format ui_short '$remote_addr $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -11,9 +12,9 @@ import os
 import gzip
 import time
 import re
-import signal
 
-from collections import defaultdict
+from copy import deepcopy
+from collections import defaultdict, namedtuple
 from datetime import date
 from argparse import ArgumentParser
 from string import Template
@@ -55,21 +56,11 @@ REPORT_FILE_NAME_FORMAT = "report-{date}.html"
 REPORT_PRECISION = 3
 
 
-PARSING_ERROR_KEY = "parsing_error"
-PARSING_SUCCESS_KEY = "parsing_success"
 MAX_PARSING_ERR_PERCENT = .5
 MIN_PARSING_ERR_COUNT = 5000
 
-REQUEST_TIME_KEY = "request_time"
-URL_KEY = "url"
-TOTAL_KEY = "total"
-COUNT_KEY = "count"
-COUNT_PERC_KEY = "count_perc"
-TIME_SUM_KEY = "time_sum"
-TIME_PERC_KEY = "time_perc"
-TIME_AVG_KEY = "time_avg"
-TIME_MAX_KEY = "time_max"
-TIME_MEDIAN_KEY = "time_med"
+
+LogFileInfo = namedtuple("LogFileInfo", ("filepath", "date"))
 
 
 def get_last_logfile_path(config):
@@ -78,62 +69,37 @@ def get_last_logfile_path(config):
         raise ValueError("Config parameter 'LOG_DIR' is required")
 
     all_filenames = (LOG_FILENAME_REGEXP.fullmatch(name) for name in os.listdir(log_dir))
-    filenames = [filename.string for filename in all_filenames if filename]
-    filenames.sort()
+    filtered_files = filter(lambda file_match: file_match is not None, all_filenames)
 
-    if len(filenames) <= 0:
-        return
+    latest_logfile_info = None
+    for file_match in filtered_files:
+        match_group = file_match.groupdict()
+        log_date_string = match_group.get("log_date")
+        log_date = time.strptime(log_date_string, "%Y%m%d")
 
-    last_logfile = filenames.pop()
-    full_path = os.path.join(log_dir, last_logfile)
-    return full_path
+        if latest_logfile_info is None or log_date > latest_logfile_info.date:
+            latest_logfile_info = LogFileInfo(
+                filepath=os.path.join(log_dir, file_match.string),
+                date=log_date
+            )
 
-
-def get_date_from_log_filename(log_filename):
-    log_basename = os.path.basename(log_filename)
-    log_match = LOG_FILENAME_REGEXP.fullmatch(log_basename)
-    if log_match is None:
-        message = "Invalid file name - {0}".format(log_filename)
-        raise ValueError(message)
-
-    log_groups = log_match.groupdict()
-    log_date_string = log_groups.get("log_date")
-    log_date = time.strptime(log_date_string, "%Y%m%d")
-    return log_date
+    return latest_logfile_info
 
 
-def is_processed_log(last_log_file, config):
-    result_dir = config.get("REPORT_DIR")
-    if result_dir is None:
-        raise ValueError("Config parameter 'REPORT_DIR' is required")
-
-    os.makedirs(result_dir, mode=DEFAULT_FS_MODE, exist_ok=True)
-
-    all_filenames = (REPORT_FILENAME_REGEXP.fullmatch(name) for name in os.listdir(result_dir))
-    filenames = [filename.string for filename in all_filenames if filename]
-    if len(filenames) <= 0:
-        return False
-
-    log_date = get_date_from_log_filename(last_log_file)
-    report_file_name = REPORT_FILE_NAME_FORMAT.format(date=time.strftime("%Y.%m.%d", log_date))
-
-    if report_file_name in filenames:
-        return True
-
-    return False
-
-
-def parse_config():
+def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument("--config", default=DEFAULT_CONFIG_PATH, help="Path to config file")
-    options = parser.parse_args()
+    cmd_arguments = parser.parse_args()
+    return cmd_arguments
 
-    config = DEFAULT_CONFIG.copy()
 
-    if not os.path.exists(options.config):
-        return config
+def parse_config(cmd_arguments):
+    config = deepcopy(DEFAULT_CONFIG)
 
-    with open(options.config, "r") as config_file:
+    if not os.path.exists(cmd_arguments.config):
+        raise FileNotFoundError("Config {0} does not exist".format(cmd_arguments.config))
+
+    with open(cmd_arguments.config, "r") as config_file:
         data = config_file.read()
         user_config = json.loads(data)
         config.update(user_config)
@@ -141,16 +107,14 @@ def parse_config():
     return config
 
 
-def setup_logger(config):
+def get_logger(config):
+    filepath = None
     log_dir = config.get("ANALYZER_LOG_DIR")
-    if log_dir is None:
-        logging.basicConfig(format=LOGGER_FORMAT, datefmt=LOGGER_DATE_FORMAT, level=logging.INFO)
-        return
-
-    os.makedirs(log_dir, mode=DEFAULT_FS_MODE, exist_ok=True)
-    current_date = date.today().strftime("%Y%m%d")
-    filename = "log_analyzer_{0}.log".format(current_date)
-    filepath = os.path.join(log_dir, filename)
+    if log_dir is not None:
+        os.makedirs(log_dir, mode=DEFAULT_FS_MODE, exist_ok=True)
+        current_date = date.today().strftime("%Y%m%d")
+        filename = "log_analyzer_{0}.log".format(current_date)
+        filepath = os.path.join(log_dir, filename)
 
     logging.basicConfig(
         format=LOGGER_FORMAT,
@@ -182,9 +146,6 @@ def get_median(times):
     if length <= 0:
         return
 
-    if length == 1:
-        return times[0]
-
     if length % 2 == 1:
         median_index = length // 2
         return times[median_index]
@@ -202,124 +163,103 @@ def get_stat_from_file(log_file, config, logger):
     total_stat = defaultdict(float)
 
     report_size = config.get("REPORT_SIZE")
-
     if report_size is None or report_size <= 0:
         raise ValueError("Config parameter 'REPORT_SIZE' is required")
 
     # Парсим построчно файл, считаем метрики которые можем посчитать на лету
     for line in log_file:
-        err_percent = parse_stat[PARSING_ERROR_KEY] / (parse_stat[PARSING_SUCCESS_KEY] or 1)
-        if parse_stat[PARSING_ERROR_KEY] >= MIN_PARSING_ERR_COUNT and err_percent >= MAX_PARSING_ERR_PERCENT:
-            message = "exceed max paring errors limit: errors {0:.0f}, success {1:.0f}".format(
-                parse_stat[PARSING_ERROR_KEY],
-                parse_stat[PARSING_SUCCESS_KEY]
-            )
-            logger.error(message)
-            return
-
         line_match = LOG_LINE_REGEXP.fullmatch(line.strip())
         if line_match is None:
-            parse_stat[PARSING_ERROR_KEY] += 1
+            parse_stat["parsing_error"] += 1
             continue
 
         line_groups = line_match.groupdict()
-        request_time_str = line_groups.get(REQUEST_TIME_KEY, "")
-        url = line_groups.get(URL_KEY)
+        request_time_str = line_groups.get("request_time", "")
+        url = line_groups.get("url")
         if url is None:
-            parse_stat[PARSING_ERROR_KEY] += 1
+            parse_stat["parsing_error"] += 1
             continue
 
         try:
             request_time = float(request_time_str)
         except ValueError:
-            parse_stat[PARSING_ERROR_KEY] += 1
+            parse_stat["parsing_error"] += 1
             continue
 
-        try:
-            log_stat[url][COUNT_KEY] += 1
-        except KeyError:
-            log_stat[url][COUNT_KEY] = 1.0
+        if "count" not in log_stat[url]:
+            log_stat[url]["count"] = 1.0
+        else:
+            log_stat[url]["count"] += 1
 
-        try:
-            log_stat[url][TIME_SUM_KEY] += request_time
-        except KeyError:
-            log_stat[url][TIME_SUM_KEY] = request_time
+        if "time_sum" not in log_stat[url]:
+            log_stat[url]["time_sum"] = request_time
+        else:
+            log_stat[url]["time_sum"] += request_time
 
-        if TIME_MAX_KEY not in log_stat[url] or request_time > log_stat[url][TIME_MAX_KEY]:
-            log_stat[url][TIME_MAX_KEY] = request_time
+        if "time_max" not in log_stat[url] or request_time > log_stat[url]["time_max"]:
+            log_stat[url]["time_max"] = request_time
 
-        if URL_KEY not in log_stat[url]:
-            log_stat[url][URL_KEY] = url
+        if "url" not in log_stat[url]:
+            log_stat[url]["url"] = url
 
-        total_stat[COUNT_KEY] += 1
-        total_stat[TIME_SUM_KEY] += request_time
+        total_stat["count"] += 1
+        total_stat["time_sum"] += request_time
         request_times[url].append(request_time)
 
-        parse_stat[PARSING_SUCCESS_KEY] += 1
+        parse_stat["parsing_success"] += 1
 
-    err_percent = parse_stat[PARSING_ERROR_KEY] / (parse_stat[PARSING_SUCCESS_KEY] or 1)
+    err_percent = parse_stat["parsing_error"] / (parse_stat["parsing_success"] or 1)
     if err_percent >= MAX_PARSING_ERR_PERCENT:
         message = "exceed max paring errors limit: errors {0:.0f}, success {1:.0f}".format(
-            parse_stat[PARSING_ERROR_KEY],
-            parse_stat[PARSING_SUCCESS_KEY]
+            parse_stat["parsing_error"],
+            parse_stat["parsing_success"]
         )
         logger.error(message)
         return
 
-    result = [url_stat for url_stat in log_stat.values()]
-    result.sort(key=lambda url_stat: url_stat[TIME_SUM_KEY], reverse=True)
+    result = sorted(log_stat.values(), key=lambda url_stat: url_stat["time_sum"], reverse=True)
     result = result[:report_size]
 
     # Для первых n url-в с наибольшим суммарным временем обработки запросов, считаем метрики которые нельзя
     # посчитать по ходу парсинга (avg, медианы и т.д.)
     for url_stat in result:
-        count_perc = url_stat[COUNT_KEY] / total_stat[COUNT_KEY] * 100
-        time_perc = url_stat[TIME_SUM_KEY] / total_stat[TIME_SUM_KEY] * 100
-        time_avg = url_stat[TIME_SUM_KEY] / url_stat[COUNT_KEY]
+        count_perc = url_stat["count"] / total_stat["count"] * 100
+        time_perc = url_stat["time_sum"] / total_stat["time_sum"] * 100
+        time_avg = url_stat["time_sum"] / url_stat["count"]
 
-        url_stat[COUNT_PERC_KEY] = round(count_perc, REPORT_PRECISION)
-        url_stat[TIME_PERC_KEY] = round(time_perc, REPORT_PRECISION)
-        url_stat[TIME_AVG_KEY] = round(time_avg, REPORT_PRECISION)
-        url_stat[TIME_SUM_KEY] = round(url_stat[TIME_SUM_KEY], REPORT_PRECISION)
+        url_stat["count_perc"] = round(count_perc, REPORT_PRECISION)
+        url_stat["time_perc"] = round(time_perc, REPORT_PRECISION)
+        url_stat["time_avg"] = round(time_avg, REPORT_PRECISION)
+        url_stat["time_sum"] = round(url_stat["time_sum"], REPORT_PRECISION)
 
-        url = url_stat[URL_KEY]
+        url = url_stat["url"]
         request_times[url].sort()
         median = get_median(request_times[url])
         if median is not None:
-            url_stat[TIME_MEDIAN_KEY] = round(median, REPORT_PRECISION)
+            url_stat["time_med"] = round(median, REPORT_PRECISION)
         else:
-            url_stat[TIME_MEDIAN_KEY] = None
+            url_stat["time_med"] = None
 
     return result
 
 
-def process_analyzing(log_file, config, logger):
-    if log_file.endswith(".gz"):
-        with gzip.open(log_file, "rt") as log:
-            return get_stat_from_file(log, config, logger)
+def process_analyzing(last_logfile_info, config, logger):
+    log_file = last_logfile_info.filepath
+    try:
+        log_file = gzip.open(log_file, "rt") if log_file.endswith(".gz") else open(log_file, "r")
+        stat = get_stat_from_file(log_file, config, logger)
+    finally:
+        log_file.close()
 
-    with open(log_file, "r") as log:
-        return get_stat_from_file(log, config, logger)
+    return stat
 
 
-def create_report(stat, config, last_log_file, logger):
-    report_dir = config.get("REPORT_DIR")
-    if report_dir is None:
-        raise ValueError("Config parameter 'REPORT_DIR' is required")
-
+def create_report(stat, config, report_path, logger):
     template_path = config.get("REPORT_TEMPLATE")
     if template_path is None:
         raise ValueError("Config parameter 'REPORT_TEMPLATE' is required")
 
-    os.makedirs(report_dir, mode=DEFAULT_FS_MODE, exist_ok=True)
-
-    log_date = get_date_from_log_filename(last_log_file)
-    report_filename = REPORT_FILE_NAME_FORMAT.format(date=time.strftime("%Y.%m.%d", log_date))
-    report_path = os.path.join(report_dir, report_filename)
-
-    if os.path.exists(report_path):
-        message = "File {0} already exists".format(report_path)
-        raise FileExistsError(message)
+    os.makedirs(os.path.dirname(report_path), mode=DEFAULT_FS_MODE, exist_ok=True)
 
     with open(template_path) as template_file:
         report_content = template_file.read()
@@ -333,45 +273,43 @@ def create_report(stat, config, last_log_file, logger):
     logger.info("Report successfully created - {0}".format(report_path))
 
 
-def get_signals_handler(logger, current_signal_handler):
-    def handler(signum, frame):
-        logger.error("Got signal {0}".format(signum))
-        current_signal_handler(signum, frame)
+def main(config, logger):
+    last_logfile_info = get_last_logfile_path(config)
 
-    return handler
-
-
-def main():
-    config = parse_config()
-    logger = setup_logger(config)
-
-    try:
-        signal.signal(signal.SIGINT, get_signals_handler(logger, signal.getsignal(signal.SIGINT)))
-        signal.signal(signal.SIGTERM, get_signals_handler(logger, signal.getsignal(signal.SIGTERM)))
-
-        last_log_file = get_last_logfile_path(config)
-
-        if last_log_file is None:
-            logger.info("No logs for analyzing")
-            return
-
-        if is_processed_log(last_log_file, config):
-            logger.info("No logs for processing")
-            return
-
-        logger.info("Start processing {0}".format(last_log_file))
-        stat = process_analyzing(last_log_file, config, logger)
-        logger.info("Statistic collected for {0}".format(last_log_file))
-
-        create_report(stat, config, last_log_file, logger)
+    if last_logfile_info is None:
+        logger.info("No logs for analyzing")
         write_complete_timestamp(config)
+        return
 
-        logger.info("Processing successfully completed")
+    report_dir = config.get("REPORT_DIR")
+    if report_dir is None:
+        raise ValueError("Config parameter 'REPORT_DIR' is required")
 
-    except Exception as e:
-        logging.exception("Runtime error: %s", e)
-        exit(1)
+    report_filename = REPORT_FILE_NAME_FORMAT.format(date=time.strftime("%Y.%m.%d", last_logfile_info.date))
+    report_path = os.path.join(report_dir, report_filename)
+
+    if os.path.exists(report_path):
+        logger.info("last log file {0} already processed".format(last_logfile_info.filepath))
+        write_complete_timestamp(config)
+        return
+
+    logger.info("Start processing {0}".format(last_logfile_info.filepath))
+    stat = process_analyzing(last_logfile_info, config, logger)
+    logger.info("Statistic collected for {0}".format(last_logfile_info.filepath))
+
+    create_report(stat, config, report_path, logger)
+    write_complete_timestamp(config)
+
+    logger.info("Processing successfully completed")
 
 
 if __name__ == "__main__":
-    main()
+    cmd_arguments = parse_arguments()
+    config = parse_config(cmd_arguments=cmd_arguments)
+    logger = get_logger(config)
+
+    try:
+        main(config, logger)
+    except:
+        logging.exception("Runtime error: ")
+        raise
